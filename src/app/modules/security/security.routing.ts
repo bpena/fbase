c9d889867ba5e3b57717e242199cc96289dd96cc
bpena <bernardo.pena.ramos@gmail.com>
2018-04-22 18:22:41 -0400

    use LoggedGuard

--- src/app/modules/security/security.routing.ts
+++ src/app/modules/security/security.routing.ts
@@ -2,7 +2,6 @@ import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { AuthModule } from "@security/auth/auth.module";
 import { UserModule } from "@security/user/user.module";
-import { AuthGuard } from "@security/auth/services/auth.guard";
 
 const ROUTES: Routes = [
 ];
--- src/app/modules/security/user/user.routing.ts
+++ src/app/modules/security/user/user.routing.ts
@@ -1,15 +1,15 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { ProfileComponent } from "@security/user/views/profile/profile.component";
-import { AuthGuard } from "@security/auth/services/auth.guard";
 import { UserComponent } from "@security/user/user.component";
+import { LoggedGuard } from "@security/auth/services/logged.guard";
 
 const ROUTES: Routes = [
     {
         path: 'user',
         component: UserComponent,
         children: [
-            { path: 'profile', component: ProfileComponent, canActivate: [AuthGuard] },
+            { path: 'profile', component: ProfileComponent, canActivate: [LoggedGuard] },
         ]
     },
 ];
